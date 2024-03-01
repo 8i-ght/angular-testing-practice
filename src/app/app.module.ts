da2f5c2d775af2848293d7e56811c96283402154
8i-ght <needboosts3545@gmail.com>
2024-03-01 14:12:22 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserTestingComponent } from './user-testing/user-testing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserTestingComponent
   ],
   imports: [
     BrowserModule,
